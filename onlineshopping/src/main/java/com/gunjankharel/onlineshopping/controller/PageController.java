a3dd1ccb1453cd2be63dced53fc4e2a951e8a7ee
Gunjan Kharel <gunjan.kharel31@gmail.com>
2018-03-14 16:16:10 +0545

    Added 2 new views. About and Contact.

--- onlineshopping/src/main/java/com/gunjankharel/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/gunjankharel/onlineshopping/controller/PageController.java
@@ -1,7 +1,6 @@
 package com.gunjankharel.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,34 +9,26 @@ public class PageController {
 	
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
-		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring MVC!");
-		
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
-//	@RequestMapping(value = {"/test"})
-//	public ModelAndView test(@RequestParam(value="greeting" ,required=false)String greeting) {
-//		if (greeting == null) {
-//			greeting = "Hello there!";
-//		}
-//		
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//		
-//		return mv;
-//	}
-	
-	@RequestMapping(value = {"/test/{greeting}"})
-	public ModelAndView test(@PathVariable("greeting")String greeting) {
-		if (greeting == null) {
-			greeting = "Hello there!";
-		}
-		
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact");
 		
+		mv.addObject("userClickContact", true);
 		return mv;
 	}
 }
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,12 @@
+<div class="container">
+	<div class="row">
+		<h1>Contact!</h1>
+		<hr/>
+		<p>
+			This is a contact page for online shopping project.
+			Phone No. : xxx-1234x-1234
+			Email: mail@xyz.com
+		</p>
+		
+	</div>
+</div>
